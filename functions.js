5b8428373cc384f880edb6e027472a502d7ef122
RockyTV <rockytvbr@gmail.com>
2013-11-15 18:13:44 -0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -12,11 +12,44 @@ var mass = document.getElementById("mass");
 var label = document.getElementById("twr");
 var msg = document.getElementById("twrmsg");
 
-
-
-if (planet.value.toLowerCase() == "kerbin")
+switch (planet.value.toLowerCase())
 {
-	gravity = 9.81;
+	case "kerbol":
+		gravity = 17.1;
+	case "moho":
+		gravity = 2.70;
+	case "eve":
+		gravity = 16.7;
+	case "gilly":
+		gravity = 0.049;
+	case "kerbin":
+		gravity = 9.81;
+	case "moon":
+	case "mün":
+	case "mun":
+		gravity = 1.63;
+	case "duna":
+		gravity = 2.94;
+	case "ike":
+		gravity = 1.10;
+	case "dres":
+		gravity = 1.13;
+	case "jool":
+		gravity = 7.85;
+	case "laythe":
+		gravity = 7.85;
+	case "vall":
+		gravity = 2.31;
+	case "tylo":
+		gravity = 7.85;
+	case "bop":
+		gravity = 0.589;
+	case "pol":
+		gravity = 0.373;
+	case "eeloo":
+		gravity = 1.69;
+	default:
+		gravity = 9.81;
 }
 
 var result = thrust.value / (mass.value * gravity); // Tsiolkovsky's rocket equation
@@ -24,14 +57,24 @@ label.innerHTML = result;
 
 if(result > 1)
 {
+<<<<<<< HEAD
 	msg.innerHTML.fontcolor("green"); // not working, need to know why
 	msg.innerHTML = capitalize(planet.value) + " Liftoff: true";
+=======
+	
+	msg.innerHTML = capitalize(planet.value) + " Liftoff: true"; 
+	msg.style.color="green";
+>>>>>>> switch statement added
 	
 	}
 else if(result < 1)
 {
 	msg.innerHTML = capitalize(planet.value) + " Liftoff: false"; 
+<<<<<<< HEAD
 	msg.innetHTML.fontcolor("red"); // not working, need to know why
+=======
+	msg.style.color="crimson";
+>>>>>>> switch statement added
 }
 
 //alert('works');
